7acb262a163637c69d1cc4174df21bfcd3a0eefa
wangzhi <wangzhione@163.com>
2017-07-24 18:29:46 +0800

    c md5 hello world

---
+++ md5utl/main.c
@@ -0,0 +1,16 @@
+﻿#include "md5util.h"
+#include <stdio.h>
+#include <string.h>
+
+//
+// md5 use test, 韦易笑绝逼是个高手
+//
+int main(int argc, char * argv[]) {
+	const char * heoo = "hello world";
+	md5s_t ms;
+
+	md5_strs(heoo, strlen(heoo), ms);
+	printf("[%s] => [%s]\n", heoo, ms);
+
+	return 0;
+}
